b23f9f0ae4706afb2bb25d8ababa345a4bc2eba6
はるか <73038051+Harukaa428@users.noreply.github.com>
2024-07-30 22:00:08 +0700

    Update a.sh
    Signed-off-by: はるか <73038051+Harukaa428@users.noreply.github.com>

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
-# sync
+# Sync
 /opt/crave/resync.sh
 
 rm -rf vendor/aosp/signing/keys
@@ -7,7 +7,7 @@ git clone https://github.com/rktdnt/p3 -b pos vendor/aosp/signing/keys
 #rm -rf vendor/derp/signing/keys
 #git clone https://github.com/rktdnt/p3 -b pos vendor/derp/signing/keys
 
-# trees
+# Tree
 rm -rf */xiaomi
 git clone --depth=1 https://github.com/Harukaa428/device_xiaomi_vayu -b 14-derp
 git clone --depth=1 https://github.com/bagaskara815/vendor_xiaomi_vayu -b 14-derp
@@ -16,10 +16,9 @@ git clone --depth=1 https://github.com/PixelOS-AOSP/hardware_xiaomi -b fourteen
 git clone --depth=1 https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_miuicamera -b 14.0
 
 # Set up build environment
-export BUILD_USERNAME=Moonlxght0909 
-export BUILD_HOSTNAME=crave
-source build/envsetup.sh
-
+ export BUILD_USERNAME=Moonlxght0909 
+ export BUILD_HOSTNAME=crave 
+ source build/envsetup.sh && 
+ 
 # Build the ROM
-lunch aosp_vayu-ap2a-user
-make installclean && mka bacon
+lunch aosp_vayu-ap2a-user && make installclean && mka bacon
