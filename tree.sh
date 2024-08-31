0714b919e21c95e5ed973c55bac12c22d0683d77
はるか <73038051+Harukaa428@users.noreply.github.com>
2024-08-31 15:34:16 +0700

    Update tree.sh
    Signed-off-by: はるか <73038051+Harukaa428@users.noreply.github.com>

--- tree.sh
+++ tree.sh
@@ -1,7 +1,6 @@
 # trees
 rm -rf */xiaomi
-git clone --depth=1 https://github.com/Harukaa428/device_xiaomi_vayu -b 14-derp
-git clone --depth=1 https://gitlab.com/bagaskara815/vendor_xiaomi_vayu -b 14-derp
-git clone --depth=1 https://github.com/bagaskara815/kernel_xiaomi_sm8150.git -b lineage-20
+git clone --depth=1 https://github.com/PixelOS-Devices/device_xiaomi_vayu -b fourteen
+git clone --depth=1 https://github.com/PixelOS-Devices/vendor_xiaomi_vayu -b fourteen
+git clone --depth=1 https://github.com/PixelOS-Devices/kernel_xiaomi_vayu.git -b fourteen
 git clone --depth=1 https://github.com/PixelOS-AOSP/hardware_xiaomi.git -b fourteen 
-git clone --depth=1 https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_miuicamera -b 14.0
